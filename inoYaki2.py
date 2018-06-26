e4f28d0461f567a5af033d50247e32cf3321965b
Nasa Matsumoto <labrador1592mary@gmail.com>
2018-06-26 21:21:50 +0900

    new code

--- inoYaki2.py
+++ inoYaki2.py
@@ -59,8 +59,6 @@ def annealingoptimize(cities,firstTour,allDist,distGreedy,T=100000, cool=0.9999)
             #値を交換する二つのindexの組み合わせの決め方をinoYakiとは変えてみた
             #やっていることは、　ランダムに一点を選んで、その一点のある程度そばにある点の中からもう一点選んで交換してみるという感じ
             #個人的にはこっちの方が焼きなまし法っぽくっていいのかなあと思ったんだけど実際どうなんだろう
-            #index: 選ばれた道順内での周り順の通し番号
-            #a, b: 道順のリストから指定されたindexの値を取り出して得られる都市番号
             citiesNumber=len(cities)
             citiesNumberIndex=(list(range(0,citiesNumber)))
             choicedCombi=random.sample(citiesNumberIndex,1)
@@ -78,7 +76,7 @@ def annealingoptimize(cities,firstTour,allDist,distGreedy,T=100000, cool=0.9999)
 
             for city in tour:
                 calculatedTour.append(city)
-
+            
             calculatedTour[index1[0]]=a
             calculatedTour[index0]=b
             #このcalculatedTourがテキトーに二点のcityを入れ替えた後の道順
@@ -92,7 +90,34 @@ def annealingoptimize(cities,firstTour,allDist,distGreedy,T=100000, cool=0.9999)
                 totalDist=newTotalDist
 
             T=T*cool
-
+        forSaiki=0
+        while forSaiki<10000:
+            citiesNumber=len(cities)
+            citiesNumberIndex=(list(range(0,citiesNumber-3)))
+            choicedCombi=random.sample(citiesNumberIndex,1)
+            index0=choicedCombi[0]
+            citiesNumberIndex=(list(range(index0+2,citiesNumber-1)))
+            choicedCombi1=random.sample(citiesNumberIndex,1)
+            index1=choicedCombi1[0]
+            
+            before=distance(cities[tour[index0]],cities[tour[index0+1]])+distance(cities[tour[index1]],cities[tour[index1+1]])
+            after=distance(cities[tour[index0]],cities[tour[index1]])+distance(cities[tour[index0+1]],cities[tour[index1+1]])
+            if before>after:
+                calculatedTour=[]
+                for city in tour[:index0+1]:
+                    calculatedTour.append(city)
+                for city in reversed(tour[index0+1:index1+1]):
+                    calculatedTour.append(city)
+                #print(calculatedTour)
+                for city in tour[index1+1:]:
+                    calculatedTour.append(city)
+                newTotalDist=calcuDist(cities,calculatedTour)
+                tour=calculatedTour
+                totalDist=newTotalDist
+                print(totalDist)
+            forSaiki+=1
+            
+        
         if totalDist<distGreedy:#Greedyより結果が良かったら終了する
             print("--------the best tour by hill climb---------")
             print(tour)
@@ -102,14 +127,14 @@ def annealingoptimize(cities,firstTour,allDist,distGreedy,T=100000, cool=0.9999)
         forSaiki+=1
         if forSaiki==100000:
             print("break")
-
+        
 
 #----------------------------↓forMain ------------------------------
 if __name__ == '__main__':
     #assert len(sys.argv) > 1
     #tour = solve(read_input(sys.argv[1]))
     #print_tour(tour)
-    #random.seed(0) #ランダム値を固定する場合
+    random.seed(0)
     assert len(sys.argv) > 1
     cities=read_input(sys.argv[1])
     tourGreedy = solve(cities)
@@ -120,7 +145,7 @@ if __name__ == '__main__':
     firstTour=makeTour(cities)
     firstDist=calcuDist(cities,firstTour)
     annealingoptimize(cities,firstTour,firstDist,distGreedy)
-
+   
 #----------------------------↑forMain------------------------------
 
 
